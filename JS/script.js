1e2ac98a981705b0e83d6fa00c4c705d67791f1c
AKeyframe <kagebro@gmail.com>
2021-09-13 17:21:25 -0500

    added the <form> and completed Book data incoperation

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,15 @@ const myKey = "AIzaSyB8eDxehjMJ-G-cGeKJb0HfPK4J0owl-yk";
 //constructor funtion supplies the paramters
  //every time a new book is created. 
 class book {
-    constructor(title, author, cover, description){
+    constructor(title, author, cover, description, pageCount, published, isbn){
         this.title = title;
         this.author = author;
         this.cover = cover;
-        this.description = description;  
+        this.description = description;
+        this.pageCount = pageCount;
+        this.published = published;
+        this.isbn = isbn;
+          
     }
 
     addToPage(){
@@ -43,7 +47,8 @@ let searchResults = [];
 ////////////////////////////////////////////////////////////
 
 //On Search button click get the value of 
-$inpDivEle.on("click", "button", function(event){
+$inpDivEle.on("submit", function(event){
+    event.preventDefault();
     resetResults();
     // If nothing was typed in : do nothing
     if($inpSearchEle.val()===""){
@@ -84,7 +89,7 @@ function ajaxCall(){
             (data) => {
                 console.log(data);
                 if(data.totalItems !== 0){    
-                    jsonToBook(data);
+                    jsonToBook(data.items);
                 }
                 else {
                     displayNoResults();
@@ -106,7 +111,7 @@ function ajaxCall(){
         promise.then(
             (data) => {
                 console.log(data);
-                jsonToBook(data);
+                jsonToBook(data.items);
             },
 
             (error) => {
@@ -122,23 +127,62 @@ function ajaxCall(){
 
 function jsonToBook(data){
     let defaultCoverImage = "/Images/noImageThumbnail.jpg";
-    for(let i=0; i<data.items.length; i++){
-        
+    
+
+    for(let i=0; i<data.length; i++){
+        let isbns = getISBNS(data[i].volumeInfo.industryIdentifiers);
         const tempBook = new book(
-            data.items[i].volumeInfo.title,
-            data.items[i].volumeInfo.authors,
-            data.items[i].volumeInfo.imageLinks?.thumbnail,
-            data.items[i].volumeInfo.description);
+            data[i].volumeInfo.title,
+            data[i].volumeInfo.authors,
+            data[i].volumeInfo.imageLinks?.thumbnail,
+            data[i].volumeInfo.description,
+            data[i].volumeInfo.pageCount,
+            data[i].volumeInfo.publishedDate,
+            isbns);
+
+            // the ? next to imageLinks is called optional chaining
         
         if(typeof tempBook.cover === "undefined"){
-            console.log("---------------------------");
             tempBook.cover = defaultCoverImage;
         }
-        console.log(tempBook.cover);
+        console.log("BOOK OBJECT   "+tempBook.pageCount);
         searchResults.push(tempBook);
     }//For end
     displayResults(searchResults);
 } //jsonToBook
+
+
+//Checks to see what ISBN is associated with the book
+//Returns eeither NO ISBN, ISBN 10, ISBN 13 or ISBN 10+13
+function getISBNS(data){
+    let totalISBN="";
+
+    //If there is no ISBN
+    if(data.length === 0){
+        return "No ISBN";
+    }
+
+    //If only 1 check the type and return
+    else if(data.length === 1){
+        if(data[0].type === "ISBN_10"){
+            return "ISBN10: "+data.identifier;
+        }
+        else{ return "ISBN13: "+data.identifier;}
+    }
+
+    //Otherwise return both
+    else{
+        for(let i=0; i<data.length; i++){
+            if(data[i].type === "ISBN_10"){
+                totalISBN+="ISBN10: "+data[i].identifier+" | ";
+            }
+            else{
+                totalISBN+="ISBN13: "+data[i].identifier+" | ";
+            }
+        }//for
+    }//else
+    return totalISBN;
+}// getISBN()
 
 
 
@@ -148,7 +192,7 @@ function jsonToBook(data){
 
 function displayResults(results){
     for(let i=0; i<results.length; i++){
-        let $tempEle = $(`<img src="${results[i].cover}"><br><li>Title: ${results[i].title} <br> Author: ${results[i].author[0]}<br><br>Description: ${results[i].description}</li>`);
+        let $tempEle = $(`<img src="${results[i].cover}"><br><li>Title: ${results[i].title} <br> Author: ${results[i].author[0]}<br>Description: ${results[i].description}<br># Of Pages: ${results[i].pageCount}<br>Publish Date: ${results[i].published}<br>${results[i].isbn}</li>`);
         $resultsEle.append($tempEle);
 
         //
@@ -169,6 +213,8 @@ function displayNoResults(){
     let tempEle = $("<h2 id=`resultsErrorNone`>No Results Found</h2>");
     $resultsEle.append(tempEle);
 }
+
+
 
 //Autocomplete
 //inputDiv event listener on keypress 
